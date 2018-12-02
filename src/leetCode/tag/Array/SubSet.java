70199d32198d8c6b9484d63e9bb24e5ca64c75a3
qxzhou1010 <qxzhou1010@qq.com>
2018-12-02 12:43:01 +0800

    LinkedList 78SubSet完整代码

--- src/leetCode/tag/Array/SubSet.java
+++ src/leetCode/tag/Array/SubSet.java
@@ -4,44 +4,38 @@ package leetCode.tag.Array;
 78 Subsets
  */
 
-import java.lang.reflect.UndeclaredThrowableException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class SubSet {
     public static void main(String[] args) {
+        SubSet subSet = new SubSet();
         List<List<Integer>> test = new ArrayList<>();
-        //test.add(nums);
+        int[] nums = {1,2,3};
+        test = subSet.subsets(nums);
         System.out.println(test);
     }
 
     public List<List<Integer>> subsets(int[] nums){
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        int target = 0;
-        backTrack(list, new ArrayList<>(), nums, target, 0);
+        backTrack(list, new ArrayList<>(), nums,0);
         // list = deleteDuplicates(list);
         return list;
     }
 
 
-    private void backTrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start){
-        if(remain < 0) return;//边界条件
-        else if ( remain == 0) list.add( new ArrayList<>(tempList));//正确解
-        else{
+    private void backTrack(List<List<Integer>> list, List<Integer> tempList, int[] nums,  int start){
+            list.add( new ArrayList<>(tempList));
+             //System.out.println(list);
             //深度搜索
             for ( int i = start; i<nums.length; i++){
                 tempList.add(nums[i]);
-
-                backTrack(list, tempList, nums, remain - nums[i],i );
-                tempList.remove(tempList.size() - 1);
+                backTrack(list, tempList, nums, i+1 );
+               tempList.remove(tempList.size() - 1);
             }
         }
-
     }
 
 
-
-
-}
